959118bd7e7a9bf98e84c8a80575b32c9ee77f5d
yungtaco <dvhyun@gmail.com>
2024-03-06 18:09:10 -0500

    Separated header and navigation components within Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,22 +3,29 @@ import { Anchor, Row, Col } from 'antd';
 
 const Header = () => (
   <>
-    <h1 style={{ textAlign: 'center', fontSize: '48px', marginBottom: '-5px' }}>My React Portfolio</h1>
-    <Row>
+    <h1 style={{ textAlign: 'center', fontSize: '48px', marginBottom: '-5px' }}>DAVID HYUN</h1>
+    <Navigation />
+  </>
+);
+
+const Navigation = () => (
+  <>
+     <Row>
       <Col xs={24} sm={4}>
-        <Anchor style={{ marginTop: '10px', marginBottom: '20px'}} items={[
-          { key: 'about-me', href: '#about-me', title: 'About Me' },
-          { key: 'portfolio', href: '#portfolio', title: 'Portfolio' },
-          { key: 'contact', href: '#contact', title: 'Contact' },
+        <Anchor style={{ marginTop: '10px', marginBottom: '20px' }}  items={[
+          { key: 'about-me', href: '#about-me', title: 'ABOUT ME' },
+          { key: 'portfolio', href: '#portfolio', title: 'PORTFOLIO' },
+          { key: 'contact', href: '#contact', title: 'CONTACT' },
         ]}
         />
       </Col>
       <Col xs={24} sm={20}>
-        <div id="about-me" style={{ height: '100vh', background: 'white' }} />
-        <div id="portfolio" style={{ height: '100vh', background: 'gray' }} />
-        <div id="contact" style={{ height: '100vh', background: 'black' }} />
+        <div id="about-me" style={{ height: '100vh', background: '#5BC0BE' }} />
+        <div id="portfolio" style={{ height: '100vh', background: '#3A506B' }} />
+        <div id="contact" style={{ height: '100vh', background: '#1C2541' }} />
       </Col>
     </Row>
   </>
 );
+
 export default Header;
